f569648c528890023496f0d35e763684cb99e08f
casualet <shaoyiwenet@gmail.com>
2015-12-26 11:36:20 +0800

    add sort_news_by_time to doc.py

--- rs/docs/doc.py
+++ rs/docs/doc.py
@@ -10,23 +10,15 @@ class Documents:
         self.AllNews = []
 
     def parse(self):
-
         all_content = []
         with codecs.open(self.path,'r','utf-8-sig') as lines:
-
             for lin in lines:
                 create_time = utility.split_data_by_date(lin)
                 lin = lin.strip().split()    
-                
                 userid,newsid,scan_time,title,create_time_ = lin[0],lin[1],lin[2],lin[3],lin[-1]
-            
-
                 news = News(int(userid), int(newsid), title, scan_time, [], create_time)
-
                 self.AllNews.append(news)
-
                 content = "".join(lin[4:-1])
-
                 all_content.append(content)
 
         if self.isTfidf:
@@ -52,6 +44,12 @@ class Documents:
     def get_all_info(self):
         return self.AllNews
 
+    def sort_news_by_time(in_news_list):
+        assert(len(in_news_list) >= 1)
+        in_news_list.sort(key = f)
+    def f(in_news):
+        print in_news.get_create_time()
+        return -1
 
 
 
