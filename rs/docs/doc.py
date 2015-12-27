9b410c8db4a06774b6ce3834b52af46922f4078b
casualet <shaoyiwenet@gmail.com>
2015-12-27 15:59:56 +0800

    mordify the function parse_cbr

--- rs/docs/doc.py
+++ rs/docs/doc.py
@@ -28,23 +28,21 @@ class Documents:
 
     def parse_cbr(self):
         all_content = []
-
         with codecs.open(self.path,'r','utf-8-sig') as lines:
             for lin in lines:
-
-                # create_time = utility.split_data_by_date(lin)
+                create_time = utility.split_data_by_date(lin)
                 lin = lin.strip().split()    
                 userid,newsid,scan_time,title,create_time_ = int(lin[0]),int(lin[1]),lin[2],lin[3],lin[-1]
                 news = News(int(userid), int(newsid), title, scan_time, [], create_time_)
                 self.AllNews.append(news)
                 content = "".join(lin[4:-1])
                 all_content.append(content)
-
         if self.isTfidf:
             tags = Tfidf(all_content).derive_keyword_zh(keyword_num=5)
-
             for index in xrange(len(tags)):
                 self.AllNews[index].tags = tags[index]
+        return self.AllNews
+
 
     def parse_user(self):
         with codecs.open(self.path,'r','utf-8-sig') as lines:
